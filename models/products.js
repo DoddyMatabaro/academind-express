80f1c03fe23a523d53efde62e74fbb00624500f1
Doddy MATABARO <matabarododdy@gmail.com>
2022-10-22 13:42:07 +0200

    save data in file.json

--- models/products.js
+++ models/products.js
@@ -1,4 +1,6 @@
-const products = [];
+const console = require('console');
+const fs = require('fs');
+const path = require('path');
 
 module.exports = class Product{
     constructor (t) {
@@ -6,8 +8,35 @@ module.exports = class Product{
     }
     save(){
         products.push(this)
+        const p = path.join(
+            path.dirname(process.mainModule.filename), 
+            'data', 
+            'products.json'
+        );
+        fs.readFile(p, (err, fileContent)=>{
+            let products = []
+            if(!err){
+                 products = JSON.parse(fileContent);
+            }
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products),(err)=>{
+                console.log('====================================');
+                console.log(err);
+                console.log('====================================');
+            });
+        });
     }
     static fetchAll(){
-        return products;
+        const p = path.join(
+            path.dirname(process.mainModule.filename), 
+            'data', 
+            'products.json'
+        );
+        fs.readFile(p, (err, fileContent)=>{
+            if(err){
+                return [];
+            }
+            return JSON.parse(fileContent);
+        })
     }
 }
